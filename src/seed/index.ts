fcef227acf9d6b40648a9f9870502fdeb48a3c5c
personal <olly.marsters@yahoo.co.uk>
2024-05-26 10:27:14 +0100

    set up database

---
+++ src/seed/index.ts
@@ -0,0 +1,38 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+type User = {
+  name: string;
+  email: string;
+};
+
+const users: User[] = [
+  { name: "Tom", email: "tom@hello.com" },
+  { name: "Bob", email: "bob@hello.com" },
+  { name: "Mark", email: "mark@hello.com" },
+  { name: "Olly", email: "olly@hello.com" },
+  { name: "Hannah", email: "hannah@hello.com" },
+  { name: "Fred", email: "fred@hello.com" },
+];
+
+const main = async () => {
+  try {
+    await prisma.user.deleteMany();
+
+    await prisma.user.createMany({ data: users });
+
+    console.log(`Successfully created ${users.length} users`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
