fbc37b05de11055aa94f5b727cf2fbb994122a13
personal <olly.marsters@yahoo.co.uk>
2024-06-30 16:34:06 +0100

    pre reset of auth stuff

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 import prisma from "./lib/prisma";
+import authConfig from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
-  providers: [],
-});
+  ...authConfig,
+  secret: process.env.AUTH_SECRET,
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
